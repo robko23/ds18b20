4538998a3ee26ba8c2d01616fab04262dab5b8e4
Robin Ilyk <contact@robko.cz>
2023-10-26 21:57:55 +0200

    embassy embedded hal 1

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,8 @@
 
 //! # Test Test
 
-use embedded_hal::blocking::delay::DelayUs;
-use embedded_hal::digital::v2::{InputPin, OutputPin};
+use embassy_time::Timer;
+use embedded_hal::digital::{InputPin, OutputPin};
 use one_wire_bus::{self, Address, OneWire, OneWireError, OneWireResult};
 
 pub const FAMILY_CODE: u8 = 0x28;
@@ -53,143 +53,133 @@ impl Ds18b20 {
     /// Starts a temperature measurement for just this device
     /// You should wait for the measurement to finish before reading the measurement.
     /// The amount of time you need to wait depends on the current resolution configuration
-    pub fn start_temp_measurement<T, E>(
+    pub async fn start_temp_measurement<T, E>(
         &self,
         onewire: &mut OneWire<T>,
-        delay: &mut impl DelayUs<u16>,
     ) -> OneWireResult<(), E>
     where
         T: InputPin<Error = E>,
         T: OutputPin<Error = E>,
     {
-        onewire.send_command(commands::CONVERT_TEMP, Some(&self.address), delay)?;
+        onewire.send_command(commands::CONVERT_TEMP, Some(&self.address)).await?;
         Ok(())
     }
 
-    pub fn read_data<T, E>(
+    pub async fn read_data<T, E>(
         &self,
         onewire: &mut OneWire<T>,
-        delay: &mut impl DelayUs<u16>,
     ) -> OneWireResult<SensorData, E>
     where
         T: InputPin<Error = E>,
         T: OutputPin<Error = E>,
     {
-        let data = read_data(&self.address, onewire, delay)?;
+        let data = read_data(&self.address, onewire).await?;
         Ok(data)
     }
 
-    pub fn set_config<T, E>(
+    pub async fn set_config<T, E>(
         &self,
         alarm_temp_low: i8,
         alarm_temp_high: i8,
         resolution: Resolution,
         onewire: &mut OneWire<T>,
-        delay: &mut impl DelayUs<u16>,
     ) -> OneWireResult<(), E>
     where
         T: InputPin<Error = E>,
         T: OutputPin<Error = E>,
     {
-        onewire.send_command(commands::WRITE_SCRATCHPAD, Some(&self.address), delay)?;
-        onewire.write_byte(alarm_temp_high.to_ne_bytes()[0], delay)?;
-        onewire.write_byte(alarm_temp_low.to_ne_bytes()[0], delay)?;
-        onewire.write_byte(resolution.to_config_register(), delay)?;
+        onewire.send_command(commands::WRITE_SCRATCHPAD, Some(&self.address)).await?;
+        onewire.write_byte(alarm_temp_high.to_ne_bytes()[0]).await?;
+        onewire.write_byte(alarm_temp_low.to_ne_bytes()[0]).await?;
+        onewire.write_byte(resolution.to_config_register()).await?;
         Ok(())
     }
 
-    pub fn save_to_eeprom<T, E>(
+    pub async fn save_to_eeprom<T, E>(
         &self,
         onewire: &mut OneWire<T>,
-        delay: &mut impl DelayUs<u16>,
     ) -> OneWireResult<(), E>
     where
         T: InputPin<Error = E>,
         T: OutputPin<Error = E>,
     {
-        save_to_eeprom(Some(&self.address), onewire, delay)
+        save_to_eeprom(Some(&self.address), onewire).await
     }
 
-    pub fn recall_from_eeprom<T, E>(
+    pub async fn recall_from_eeprom<T, E>(
         &self,
         onewire: &mut OneWire<T>,
-        delay: &mut impl DelayUs<u16>,
     ) -> OneWireResult<(), E>
     where
         T: InputPin<Error = E>,
         T: OutputPin<Error = E>,
     {
-        recall_from_eeprom(Some(&self.address), onewire, delay)
+        recall_from_eeprom(Some(&self.address), onewire).await
     }
 }
 
 /// Starts a temperature measurement for all devices on this one-wire bus, simultaneously
-pub fn start_simultaneous_temp_measurement<T, E>(
+pub async fn start_simultaneous_temp_measurement<T, E>(
     onewire: &mut OneWire<T>,
-    delay: &mut impl DelayUs<u16>,
 ) -> OneWireResult<(), E>
 where
     T: InputPin<Error = E>,
     T: OutputPin<Error = E>,
 {
-    onewire.reset(delay)?;
-    onewire.skip_address(delay)?;
-    onewire.write_byte(commands::CONVERT_TEMP, delay)?;
+    onewire.reset().await?;
+    onewire.skip_address().await?;
+    onewire.write_byte(commands::CONVERT_TEMP).await?;
     Ok(())
 }
 
 /// Read the contents of the EEPROM config to the scratchpad for all devices simultaneously.
-pub fn simultaneous_recall_from_eeprom<T, E>(
+pub async fn simultaneous_recall_from_eeprom<T, E>(
     onewire: &mut OneWire<T>,
-    delay: &mut impl DelayUs<u16>,
 ) -> OneWireResult<(), E>
 where
     T: InputPin<Error = E>,
     T: OutputPin<Error = E>,
 {
-    recall_from_eeprom(None, onewire, delay)
+    recall_from_eeprom(None, onewire).await
 }
 
 /// Read the config contents of the scratchpad memory to the EEPROMfor all devices simultaneously.
-pub fn simultaneous_save_to_eeprom<T, E>(
+pub async fn simultaneous_save_to_eeprom<T, E>(
     onewire: &mut OneWire<T>,
-    delay: &mut impl DelayUs<u16>,
 ) -> OneWireResult<(), E>
 where
     T: InputPin<Error = E>,
     T: OutputPin<Error = E>,
 {
-    save_to_eeprom(None, onewire, delay)
+    save_to_eeprom(None, onewire).await
 }
 
-pub fn read_scratchpad<T, E>(
+pub async fn read_scratchpad<T, E>(
     address: &Address,
     onewire: &mut OneWire<T>,
-    delay: &mut impl DelayUs<u16>,
 ) -> OneWireResult<[u8; 9], E>
 where
     T: InputPin<Error = E>,
     T: OutputPin<Error = E>,
 {
-    onewire.reset(delay)?;
-    onewire.match_address(address, delay)?;
-    onewire.write_byte(commands::READ_SCRATCHPAD, delay)?;
+    onewire.reset().await?;
+    onewire.match_address(address).await?;
+    onewire.write_byte(commands::READ_SCRATCHPAD).await?;
     let mut scratchpad = [0; 9];
-    onewire.read_bytes(&mut scratchpad, delay)?;
+    onewire.read_bytes(&mut scratchpad).await?;
     check_crc8(&scratchpad)?;
     Ok(scratchpad)
 }
 
-fn read_data<T, E>(
+async fn read_data<T, E>(
     address: &Address,
     onewire: &mut OneWire<T>,
-    delay: &mut impl DelayUs<u16>,
 ) -> OneWireResult<SensorData, E>
 where
     T: InputPin<Error = E>,
     T: OutputPin<Error = E>,
 {
-    let scratchpad = read_scratchpad(address, onewire, delay)?;
+    let scratchpad = read_scratchpad(address, onewire).await?;
 
     let resolution = if let Some(resolution) = Resolution::from_config_register(scratchpad[4]) {
         resolution
@@ -211,37 +201,36 @@ where
     })
 }
 
-fn recall_from_eeprom<T, E>(
+async fn recall_from_eeprom<T, E>(
     address: Option<&Address>,
     onewire: &mut OneWire<T>,
-    delay: &mut impl DelayUs<u16>,
 ) -> OneWireResult<(), E>
 where
     T: InputPin<Error = E>,
     T: OutputPin<Error = E>,
 {
-    onewire.send_command(commands::RECALL_EEPROM, address, delay)?;
+    onewire.send_command(commands::RECALL_EEPROM, address).await?;
 
     // wait for the recall to finish (up to 10ms)
     let max_retries = (10000 / one_wire_bus::READ_SLOT_DURATION_MICROS) + 1;
     for _ in 0..max_retries {
-        if onewire.read_bit(delay)? == true {
+        if onewire.read_bit().await? == true {
             return Ok(());
         }
     }
     Err(OneWireError::Timeout)
 }
 
-fn save_to_eeprom<T, E>(
+async fn save_to_eeprom<T, E>(
     address: Option<&Address>,
     onewire: &mut OneWire<T>,
-    delay: &mut impl DelayUs<u16>,
 ) -> OneWireResult<(), E>
 where
     T: InputPin<Error = E>,
     T: OutputPin<Error = E>,
 {
-    onewire.send_command(commands::COPY_SCRATCHPAD, address, delay)?;
-    delay.delay_us(10000); // delay 10ms for the write to complete
+    onewire.send_command(commands::COPY_SCRATCHPAD, address).await?;
+    // delay 10ms for the write to complete
+    Timer::after_millis(10).await;
     Ok(())
 }
